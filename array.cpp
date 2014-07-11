8eff8cd83ade5e908fa94c8e4bcd67a874a86c17
Antoine Poliakov <apoliakov@aneo.fr>
2014-07-11 18:24:54 +0200

    compilesg++ -std=c++11 -fdiagnostics-color=always -Wall array.cpp 2>&1

--- array.cpp
+++ array.cpp
@@ -28,11 +28,11 @@ using uint = unsigned int;
 
 template<class T> struct TYPE { using type = T; };
 
-template<class A, class B> using common2_tt = typename std::common_type<A, B>::type;
+template<class A, class B> using common2 = typename std::common_type<A, B>::type;
 template<class... Ts> struct COMMON;
 template<class T> struct COMMON<T> : TYPE<T> {};
 template<class F, class... Ns> struct COMMON<F, Ns...> : std::common_type<F, typename COMMON<Ns...>::type> {};
-template<class... Ts> using common_tt = typename COMMON<Ts...>::type;
+template<class... Ts> using common = typename COMMON<Ts...>::type;
 
 
 enum ValueKind { sta, dyn, ref };
@@ -73,7 +73,7 @@ template<class T> using ZERO = STATIC<T, 0>;
 template<class T> using ONE = STATIC<T, 1>;
 using TRUE = STATIC<bool, true>;
 using FALSE = STATIC<bool, false>;
-template<class A, class B> using ADD = STATIC<COMMON<typename A::type, typename B::type>, A::value + B::value>;
+template<class A, class B> using ADD = STATIC<common<typename A::type, typename B::type>, A::value + B::value>;
 template<class X> using INC = STATIC<typename X::type, 1 + X::value>;
 
 struct NIL { using nil_tag = void; };
@@ -101,10 +101,10 @@ template<class A, class B> struct CAT_T;
 template<class... As, class... Bs> struct CAT_T<TYPES<As...>, TYPES<Bs...>> : TYPES<As..., Bs...> {};
 template<class A, class B> using cat_t = typename CAT_T<A, B>::types;
 
-template<class T, T... args> struct VALUES : TYPE<T> { using values = VALUES; };
+template<class T, T... args> struct VALUES : TYPE<T> { using values = VALUES<T, args...>; };
 TYPEDEF_TEST(isValues, values)
 
-template<class X, X H, class T> struct CONS_V;
+template<class X, X H, class T> struct CONS_V : CONS_V<X, H, typename T::values> {};
 template<class X, X H, X... Vs> struct CONS_V<X, H, VALUES<X, Vs...>> : VALUES<X, H, Vs...> {};
 template<class X, X H, class T> using cons_v = typename CONS_V<X, H, T>::values;
 
@@ -113,6 +113,15 @@ template<class X, X... As, X... Bs> struct CAT_V<VALUES<X, As...>, VALUES<X, Bs.
 template<class A, class B> using cat_v = typename CAT_V<A, B>::values;
 
 template<class T> constexpr bool isList() { return isValues<T>() || isTypes<T>(); }
+
+template<class L, int I> struct AT;
+template<class F, class... Ns> struct AT<TYPES<F, Ns...>, 0> : TYPE<F> {};
+template<int I, class F, class... Ns> struct AT<TYPES<F, Ns...>, I> : AT<TYPES<Ns...>, I-1> {};
+template<class T, T F, T... Ns> struct AT<VALUES<T, F, Ns...>, 0> : STATIC<T, F> {};
+template<int I, class T, T F, T... Ns> struct AT<VALUES<T, F, Ns...>, I> : AT<VALUES<T,Ns...>, I-1> {};
+template<class T, int i> using at_t = typename AT<T, i>::type;
+template<class T, int i> constexpr typename T::type at_v() { return AT<T,i>::value; }
+
 
 //#define MAYBE_TYPE(P_, T_) typename std::enable_if<P_<T>(), T>::type
 template<bool C, class T, class F> struct COND : TYPE<T> {};
@@ -129,12 +138,12 @@ template<class T> constexpr typename std::enable_if<isList<T>(), uint>::type len
 template<class T, template<class> class P> struct COUNT_T;
 template<template<class> class P> struct COUNT_T<TYPES<>, P> : ZERO<uint> {};
 template<template<class> class P, class F, class... Ns>  struct COUNT_T<TYPES<F, Ns...>, P> : STATIC<uint, (P<F>::value ? 1:0) + COUNT_T<TYPES<Ns...>, P>::value> {};
-template<class T, template<class> class P> constexpr typename std::enable_if<isList<T>(), uint>::type count() { return COUNT_T<T, P>::value; }
+template<class T, template<class> class P> constexpr typename std::enable_if<isTypes<T>(), uint>::type count_t() { return COUNT_T<T, P>::value; }
 
-template<class T, template<typename T::type> class P> struct COUNT_V;
-template<class T, template<T> class P> struct COUNT_V<VALUES<T>, P> : ZERO<uint> {};
-template<class T, template<T> class P, T F, T... Ns> struct COUNT_V<VALUES<T, F, Ns...>, P> : STATIC<uint, (P<F>::value ? 1:0) + COUNT_V<VALUES<T, Ns...>, P>::value> {};
-template<class T, template<typename T::type> class P> constexpr typename std::enable_if<isList<T>(), uint>::type count() { return COUNT_V<T, P>::value; }
+template<class T, template<class> class P> struct COUNT_V;
+template<class T, template<class> class P> struct COUNT_V<VALUES<T>, P> : ZERO<uint> {};
+template<class T, template<class> class P, T F, T... Ns> struct COUNT_V<VALUES<T, F, Ns...>, P> : STATIC<uint, (P<T>::template pred<F>::value ? 1:0) + COUNT_V<VALUES<T, Ns...>, P>::value> {};
+template<class T, template<class> class P> constexpr typename std::enable_if<isValues<T>(), uint>::type count_v() { return COUNT_V<T, P>::value; }
 
 template<class T, template<class> class P> struct FILTER_T;
 template<template<class> class P> struct FILTER_T<TYPES<>, P> : TYPES<> {};
@@ -145,14 +154,14 @@ template<template<class> class P, class F, class... Ns> struct FILTER_T<TYPES<F,
 >::type::types {};
 template<class T, template<class> class P> using filter_t = typename FILTER_T<T, P>::types;
 
-template<class T, template<typename T::type> class P> struct FILTER_V;
-template<class T, template<T> class P> struct FILTER_V<VALUES<T>, P> : VALUES<T> {};
-template<class T, template<T> class P, T F, T... Vs> struct FILTER_V<VALUES<T, F, Vs...>, P> : COND<
-	P<F>::value,
+template<class T, template<class> class P> struct FILTER_V;
+template<class T, template<class> class P> struct FILTER_V<VALUES<T>, P> : VALUES<T> {};
+template<class T, template<class> class P, T F, T... Vs> struct FILTER_V<VALUES<T, F, Vs...>, P> : COND<
+	P<T>::template pred<F>::value,
 	typename CONS_V< T, F, typename FILTER_V<VALUES<T, Vs...>, P>::values >::type,
 	typename FILTER_V<VALUES<T, Vs...>, P>::values
 >::type::values {};
-template<class T, template<typename T::type> class P> using filter_v = typename FILTER_V<T, P>::values;
+template<class T, template<class> class P> using filter_v = typename FILTER_V<T, P>::values;
 
 template<class T, template<class> class P> struct MAP_TT;
 template<template<class> class P, class... Ts> struct MAP_TT<TYPES<Ts...>, P> : TYPES< typename P<Ts>::type... > {};
@@ -162,23 +171,32 @@ template<class T, template<class> class P> struct MAP_TV;
 template<template<class> class P, class... Ts> struct MAP_TV<TYPES<Ts...>, P> : VALUES< typename P<void>::type, P<Ts>::value... > {};
 template<class T, template<class> class P> using map_tv = typename MAP_TV<T, P>::values;
 
-template<class T, template<typename T::type> class P> struct MAP_VT;
-template<class T, template<typename T::type> class P, T... Vs> struct MAP_VT<VALUES<T, Vs...>, P> : TYPES< typename P<Vs>::type... > {};
-template<class T, template<typename T::type> class P> using map_vt = typename MAP_VT<T, P>::types;
+template<class T, template<class> class P> struct MAP_VT;
+template<class T, template<class> class P, T... Vs> struct MAP_VT<VALUES<T, Vs...>, P> : TYPES< typename P<T>::template pred<Vs>::type... > {};
+template<class T, template<class> class P> using map_vt = typename MAP_VT<T, P>::types;
 
-template<class T, template<typename T::type> class P> struct MAP_VV;
-template<class T, template<typename T::type> class P, T... Vs> struct MAP_VV<VALUES<T, Vs...>, P> : VALUES< typename P<std::declval<T>()>::type, P<Vs>::value... > {};
-template<class T, template<typename T::type> class P> using map_vv = typename MAP_VV<T, P>::values;
+template<class T, template<class> class P> struct MAP_VV;
+template<class T, template<class> class P, T... Vs> struct MAP_VV<VALUES<T, Vs...>, P> : VALUES< typename P<T>::type, P<T>::template pred<Vs>::value... > {};
+template<class T, template<class> class P> using map_vv = typename MAP_VV<T, P>::values;
 
 template<class T, template<class,class> class B, class S = void> struct REDUCE_TT;
 template<template<class,class> class B, class S> struct REDUCE_TT<TYPES<>, B, S> : TYPE<S> {};
 template<template<class,class> class B, class S, class F, class... Ns> struct REDUCE_TT<TYPES<F, Ns...>, B, S> : B<F, typename REDUCE_TT<TYPES<Ns...>, B, S>::type> {};
 template<class T, template<class,class> class B, class S = void> using reduce_tt = typename REDUCE_TT<T, B, S>::type;
 
-template<class T, template<typename T::type, typename T::type> class B, typename T::type S = typename T::type()> struct REDUCE_VV;
-template<class T, template<T,T> class B, T S> struct REDUCE_VV<VALUES<T>, B, S> : VALUE<T,S> {};
-template<class T, template<T,T> class B, T S, T F, T... Ns> struct REDUCE_VV<VALUES<T, F, Ns...>, B, S> : B<F, REDUCE_VV<VALUES<T, Ns...>, B, S>::value> {};
-template<class T, template<typename T::type, typename T::type> class B, typename T::type S = typename T::type()> constexpr typename T::type reduce_vv() { return REDUCE_VV<T, B, S>::value; }
+template<class T, template<class> class B, typename T::type S = typename T::type()> struct REDUCE_VV;
+template<class T, template<class> class B, T S> struct REDUCE_VV<VALUES<T>, B, S> : VALUE<T,S> {};
+template<class T, template<class> class B, T S, T F, T... Ns> struct REDUCE_VV<VALUES<T, F, Ns...>, B, S> : B<T>::template bin<F, REDUCE_VV<VALUES<T, Ns...>, B, S>::value> {};
+template<class T, template<class> class B, typename T::type S = typename T::type()> constexpr typename T::type reduce_vv() { return REDUCE_VV<T, B, S>::value; }
+
+template<class T, template<class> class B, typename T::type S = typename T::type()> struct CUMUL;
+template<class T, template<class> class B, T S> struct CUMUL<VALUES<T>, B, S> : VALUES<T> {};
+template<class T, template<class> class B, T S, T F, T... Ns> struct CUMUL<VALUES<T, F, Ns...>, B, S> : CONS_V<T, B<T>::template bin<F,S>::value, CUMUL<VALUES<T, Ns...>, B, B<T>::template bin<F,S>::value>> {};
+template<class T, template<class> class B, typename T::type S =typename T::type()> using cumul = typename CUMUL<T, B, S>::values;
+
+template<class Vals, class Target> struct CAST_V;
+template<class T, class Target, T... Vals> struct CAST_V<VALUES<T, Vals...>, Target> : VALUES<Target, Target(Vals)...> {};
+template<class Vals, class Target> using cast_v = typename CAST_V<Vals, Target>::values;
 
 
 template<class T, ValueKind SD, T V> std::ostream & operator<<(std::ostream & s, const VALUE<T, SD, V> & val) {
@@ -202,32 +220,52 @@ template<int N> struct tuple_ostream<N,N> {
 };
 template<class... Ts> std::ostream & operator<<(std::ostream & s, const std::tuple<Ts...> & tp) {
 	s << "tuple(";
-	tuple_ostream<0, count<TYPES<Ts...>>()>::f(s, tp);
+	tuple_ostream<0, len<TYPES<Ts...>>()>::f(s, tp);
 	return s << ')';
 }
 
-template<class T> struct POSITIVE {
-	template<T x> using pred = STATIC<bool, (x >= 0)>;
+template<class T> struct POSITIVE : TYPE<bool> {
+	template<T x> struct pred : STATIC<bool, (x >= 0)> {};
 };
-template<class T> struct NEGATIVE {
-	template<T x> using pred = STATIC<bool, (x < 0)>;
+template<class T> struct NEGATIVE : TYPE<bool> {
+	template<T x> class pred : public STATIC<bool, (x < 0)> {};
 };
 template<class T> struct SUM {
 	template<T a, T b> using bin = STATIC<T, a+b>;
 };
 
-template<int... args_> struct ints {
-	using args = VALUES<int, args_...>;
-	static const uint n = count<VALUES<int, args_...>>();
-	static const uint n_static = count<VALUES<int, args_...>, POSITIVE<int>::pred>(), n_dynamic = n - n_static;
-	using dyn = typename MAP_VV<VALUES<int, args_...>, NEGATIVE<int>::pred>::values;
-	using index = typename CUMSUM<dyn>::values;
+template<int... args> struct ints {
+	using ARGS = VALUES<int, args...>;
+	static const uint n = len<ARGS>();
+	static const uint n_static = count_v<ARGS, POSITIVE>(), n_dynamic = n - n_static;
+	using DYN = map_vv<ARGS, NEGATIVE>;
+	using INDEX = cumul<cast_v<DYN, int>, SUM>;
 	uint data[n_dynamic];
 	
 	template<int i> int read() const {
-		return AT<dyn, i>::value ? data[AT<index, i>::value] : AT<args, i>::value;
+		return at_v<DYN,i>() ? data[at_v<INDEX,i>()] : at_v<ARGS,i>();
 	}
-	q
+};
+
+template<int i, bool ok, int... args> struct ints_printer {
+	static void f(std::ostream & s, const ints<args...> & vals) {
+		if(i) s << ", ";
+		s << vals.template read<i>();
+		ints_printer<i+1, i+1==ints<args...>::n, args...>::f(s, vals);
+	}
+};
+template<int i, int... args> struct ints_printer<i, false, args...> {
+	static void f(std::ostream & s, const ints<args...> & vals) {}
+};
+
+template<int... args> inline void print_ints(std::ostream & s, const ints<args...> & vals) {
+	ints_printer<0, bool(ints<args...>::n), args...>::f(s, vals);
+};
+
+template<int... args> std::ostream & operator<<(std::ostream & s, const ints<args...> & vals) {
+	s << "ints(";
+	print_ints(s, vals);
+	return s << ')';
 };
 
 
